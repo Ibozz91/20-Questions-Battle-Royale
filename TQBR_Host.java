6d890a0d83e60b42969b96cacda41c8130d352bf
Ibozz91 <ianbozzuto@gmail.com>
2022-09-24 17:27:59 -0400

    iiiiiiiiiiiiiii

--- TQBR_Host.java
+++ TQBR_Host.java
@@ -1,5 +1,11 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
 public class TQBR_Host{
     public static void main(String Args[]){
-
+        Scanner Scan = new Scanner(System.in);
+        ServerSocket ss = new ServerSocket(20200);
+        
     }
 }
