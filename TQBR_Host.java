358868a07decfa49bd6c76ef50c3f482244701f3
Ibozz91 <ianbozzuto@gmail.com>
2022-09-24 17:32:48 -0400

    better

--- TQBR_Host.java
+++ TQBR_Host.java
@@ -1,10 +1,5 @@
 import java.io.*;
 import java.net.*;
-<<<<<<< HEAD
-public class TQBR_Host{
-    public static void main(String Args[]) throws IOException{
-
-=======
 import java.util.Scanner;
 
 public class TQBR_Host{
@@ -12,6 +7,5 @@ public class TQBR_Host{
         Scanner Scan = new Scanner(System.in);
         ServerSocket ss = new ServerSocket(20200);
         
->>>>>>> 6d890a0d83e60b42969b96cacda41c8130d352bf
     }
 }
