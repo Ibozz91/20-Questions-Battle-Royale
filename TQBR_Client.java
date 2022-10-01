71b3f12b64c155c7f8ced3041471d072c9d830ba
bradyeldredge31 <bradyeldredge31@gmail.com>
2022-10-01 13:53:44 -0700

    client

--- TQBR_Client.java
+++ TQBR_Client.java
@@ -1,7 +1,24 @@
-import java.io.IOException;
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 
 public class TQBR_Client{
     public static void main(String Args[]) throws IOException{
+        Socket s = null;
+        BufferedReader br = null;
+        PrintStream ps = null;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("enter an IP address.");
+        String ip = scan.nextLine();
+        s = new Socket(ip, 20200);
+        br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        ps = new PrintStream(s.getOutputStream());
+
+        
+
+
+
+
 
     }
 }
