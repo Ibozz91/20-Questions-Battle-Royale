050fbdeaa1691a79c0307229b35d0a45b1bb14dd
Ibozz91 <ianbozzuto@gmail.com>
2022-10-01 16:14:09 -0400

    Added rules to host program

--- TQBR_Host.java
+++ TQBR_Host.java
@@ -1,3 +1,12 @@
+/*
+   TWENTY QUESTIONS BATTLE ROYALE
+   Rules:
+   Each player thinks of one thing each
+   During each turn, every uneliminated player asks a yes/no question to every other eliminated player.
+   After this, players answer questions honestly.
+   Screenpeeking, and teams are not allowed.
+   Last player remaining wins.
+ */
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
@@ -6,6 +15,7 @@ public class TQBR_Host{
     public static void main(String Args[]) throws IOException, UnknownHostException{
         Scanner Scan = new Scanner(System.in);
         ServerSocket ss = new ServerSocket(20200);
-        
+        System.out.println("Twenty Questions Battle Royale");
+
     }
 }
