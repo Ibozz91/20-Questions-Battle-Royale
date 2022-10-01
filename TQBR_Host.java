7bbfffa51fbbffcbe7d663942a8a3785a6fbd7d6
Ibozz91 <ianbozzuto@gmail.com>
2022-10-01 16:07:27 -0400

    Changed Host

--- TQBR_Host.java
+++ TQBR_Host.java
@@ -2,8 +2,8 @@ import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 
-public class TQBR_Host throws IOException{
-    public static void main(String Args[]){
+public class TQBR_Host{
+    public static void main(String Args[]) throws IOException, UnknownHostException{
         Scanner Scan = new Scanner(System.in);
         ServerSocket ss = new ServerSocket(20200);
         
