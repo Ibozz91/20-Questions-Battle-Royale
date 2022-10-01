f7c59a072f4cc7c0e6c854793642d6e9488c552c
bradyeldredge31 <bradyeldredge31@gmail.com>
2022-10-01 13:01:47 -0700

    m

--- TQBR_Host.java
+++ TQBR_Host.java
@@ -1,17 +1,10 @@
-import java.io.*;
 import java.net.*;
-<<<<<<< HEAD
-public class TQBR_Host{
-    public static void main(String Args[]) throws IOException{
-
-=======
+import java.io.*;
 import java.util.Scanner;
-
 public class TQBR_Host{
     public static void main(String Args[]){
         Scanner Scan = new Scanner(System.in);
         ServerSocket ss = new ServerSocket(20200);
         
->>>>>>> 6d890a0d83e60b42969b96cacda41c8130d352bf
     }
 }
